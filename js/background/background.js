ec767ad0f8fde79467d6ca5e66c437570019c53c
Deefoozy <tinotrok@outlook.com>
2019-10-02 14:08:24 +0200

    Removed console unnecessary console logs and fixed issue where badgetest was always set regardless of which tab

--- js/background/background.js
+++ js/background/background.js
@@ -1,18 +1,14 @@
-console.log(chrome);
 chrome.runtime.onMessage.addListener(updateCounter);
 
 function updateCounter(data, sender) {
     if (data.count)
-        console.log(sender.tab.id);
         chrome.tabs.get(sender.tab.id, tab => {
             if (!chrome.runtime.lastError) {
                 let text = `${data.count}`;
 
                 if (tab.index >= 0) {
-                    console.log(tab.id);
                     chrome.browserAction.setBadgeText({tabId: tab.id, text: text});
                 } else {
-                    console.log('else');
                     let tabId = sender.tab.id, text = text;
 
                     chrome.webNavigation.onCommitted.addListener(function update(details) {
@@ -24,5 +20,4 @@ function updateCounter(data, sender) {
                 }
             }
         });
-    chrome.browserAction.setBadgeText({text: `${data.count}`});
 }
